1a05b16a0d2663d794fb53c522e651679e95fb3b
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-28 13:06:02 +0100

    Add Category and Task controllers

---
+++ src/main/java/com/jossegonnza/todolist/controller/CategoryController.java
@@ -0,0 +1,86 @@
+package com.jossegonnza.todolist.controller;
+
+import com.jossegonnza.todolist.entity.CategoryEntity;
+import com.jossegonnza.todolist.entity.TaskEntity;
+import com.jossegonnza.todolist.entity.UserEntity;
+import com.jossegonnza.todolist.service.CategoryService;
+import com.jossegonnza.todolist.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "/categories/home";
+    }
+
+    @GetMapping("/new")
+    public String newCategoryPage() {
+        return "/categories/create-category";
+    }
+
+    @PostMapping("/create")
+    public String createCategory(CategoryEntity category, HttpSession session) {
+        UserEntity user = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString())).get();
+        category.setUser(user);
+
+        categoryService.createCategory(category);
+        return "redirect:/category/home";
+    }
+
+    @GetMapping("/categoryPage/{id}")
+    public String categoryPage(@PathVariable Long id, Model model) {
+        CategoryEntity category = categoryService.getCategoryById(id).orElseThrow(() -> new IllegalArgumentException("Invalid category ID"));
+        List<TaskEntity> tasks = category.getTasks();
+
+        model.addAttribute("category", category);
+        model.addAttribute("tasks", tasks);
+
+        return "/categories/categoryPage";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editCategory(@PathVariable Long id, Model model) {
+        CategoryEntity category = categoryService.getCategoryById(id).orElseThrow(() -> new IllegalArgumentException("Invalid category ID"));
+        model.addAttribute("category", category);
+
+        return "/categories/update-category";
+    }
+
+    @PostMapping("/update")
+    public String updateCategory(@RequestParam("idCategory") Long id, CategoryEntity category) {
+        categoryService.updateCategory(id, category);
+
+        return "/categories/home";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategoryById(id);
+
+        return ("/categories/home");
+    }
+
+    @GetMapping("/cancel/{id}")
+    public String cancelEditCategory(@PathVariable Long id) {
+        CategoryEntity category = categoryService.getCategoryById(id).orElseThrow(() -> new IllegalArgumentException("Invalid category ID"));
+
+        return "redirect/category/categoryPage";
+    }
+
+}
---
+++ src/main/java/com/jossegonnza/todolist/controller/TaskController.java
@@ -0,0 +1,74 @@
+package com.jossegonnza.todolist.controller;
+
+import com.jossegonnza.todolist.entity.CategoryEntity;
+import com.jossegonnza.todolist.entity.TaskEntity;
+import com.jossegonnza.todolist.entity.UserEntity;
+import com.jossegonnza.todolist.service.CategoryService;
+import com.jossegonnza.todolist.service.TaskService;
+import com.jossegonnza.todolist.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/addTask")
+    public String addTask(@RequestParam("taskId") Long categoryId, TaskEntity task, HttpSession session) {
+        UserEntity user = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString())).get();
+        CategoryEntity category = categoryService.getCategoryById(categoryId).orElseThrow(() -> new IllegalArgumentException("Invalid category ID"));
+
+        task.setCreatedAt(LocalDateTime.now());
+
+        taskService.createTask(task);
+
+        return "redirect:/category/categoryPage";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editTask(@PathVariable Long id, Model model) {
+        TaskEntity task = taskService.getTaskById(id).orElseThrow(() -> new IllegalArgumentException("Invalid task ID"));
+
+        model.addAttribute("task", task);
+
+        return "/tasks/update-task";
+    }
+
+    @PostMapping("/update")
+    public String updateTask(@RequestParam("taskId") Long id, TaskEntity task) {
+        TaskEntity taskDB = taskService.getTaskById(id).orElseThrow(() -> new IllegalArgumentException("Invalid task ID"));
+        taskService.updateTask(id, task);
+
+        return "redirect:/category/categoryPage" + taskDB.getCategory().getId();
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        TaskEntity task = taskService.getTaskById(id).orElseThrow(() -> new IllegalArgumentException("Invalid task ID"));
+        taskService.deleteTask(id);
+
+        return "redirect:/category/categoryPage" + task.getCategory().getId();
+    }
+
+    @GetMapping("/cancel/{id}")
+    public String cancelEditTask(@PathVariable Long id){
+        TaskEntity task = taskService.getTaskById(id).orElseThrow(() -> new IllegalArgumentException("Invalid task ID"));
+
+        return "redirect:/category/categoryPage" + task.getCategory().getId();
+    }
+
+}
