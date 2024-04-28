b35270da720b7b531693a0663b7938a66ac3419c
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-28 14:37:04 +0100

    Conected with Angular

--- src/main/java/com/jossegonnza/todolist/entity/TaskEntity.java
+++ src/main/java/com/jossegonnza/todolist/entity/TaskEntity.java
@@ -5,11 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
