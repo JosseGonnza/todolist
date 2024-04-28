8106bc0a60969af92df0c3b8992b66c0f8d43b6e
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-28 11:43:05 +0100

    Add UserController

---
+++ src/main/java/com/jossegonnza/todolist/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.jossegonnza.todolist.controller;
+
+import com.jossegonnza.todolist.entity.UserEntity;
+import com.jossegonnza.todolist.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @GetMapping("/record")
+    public String recordPage() {
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String register(UserEntity user) {
+        user.setPassword((passwordEncoder.encode(user.getPassword())));
+        userService.createUser(user);
+        return "redirect:/index";
+    }
+
+    @GetMapping(value = {"/index", "/"})
+    public String loginPage() {
+        return "/users/index";
+    }
+
+    @GetMapping("/access")
+    public String access(HttpSession session) {
+        Optional<UserEntity> optionalUser = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString()));
+
+        if (optionalUser.isPresent()) {
+            session.setAttribute("user_session_id", optionalUser.get().getId());
+            return "redirect:/post/home";
+        } else {
+            return "redirect:/index";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/index";
+    }
+}
