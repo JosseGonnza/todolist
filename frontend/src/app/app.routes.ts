fe2f7295246a4d822eadd94bb51c138282b0aa1f
JosseGonnza <accion.quevedo@hotmail.com>
2024-04-29 20:36:51 +0100

    Add login and register styles

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login.component';
+import { RegisterComponent } from './components/auth/register.component';
 
 export const routes: Routes = [
-  { path: 'index', component: LoginComponent }
+  { path: 'index', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 export default routes;
